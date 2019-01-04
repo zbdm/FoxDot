a7a30f8db71cab422016919f1dfff28c89520e43
Zbdm <39557845+zbdm@users.noreply.github.com>
2019-01-04 13:36:03 +0100

    Update Schlaffloss.py

--- docs/Schlaffloss.py
+++ docs/Schlaffloss.py
@@ -1,8 +1,4 @@
-Master().reset()
-
-IDEES GENERALES
-phase I  " postrocky"
-pad >> bass >> kraut
+Phase I  " postrocky" // pad >> bass >> kraut
 
 Clock.bpm = 90
 Scale.default.set = "minor"
@@ -17,28 +13,19 @@ d2 >> play("<X.><:=><..|o1|.><v >", amp=1).every(14.5, "jump")
 PHASE II "TECHNO !!!"
 
 Master().lpf=800
-d_all.stop()
-b1.stop()
-
-b2 >> dbass(PSine(64)*0.2, lpf=900, dur=[1/2,1/4,1/2,1/4,1/2]).sometimes("dur.shuffle")
+b2 >> dbass(PSine(64)*0.2, lpf=900, dur=[1/2, 1/4 , 1/2, 1/4, 1/2]).sometimes("dur.shuffle")
 p1 >> prof(b2.degree, oct=(var([(3,4),[5,6]],[6,2])), dur=1/2, sus=p1.dur*0.7, lforate=[1,2,4], lfowidth=0.9)
 
 Master().lpf=linvar([900,10000],[64,inf], start=now)
 Clock.bpm=linvar([90,120],[64,inf], start=now)
-Group(a1,s1,s2).stop()
 
 d3 >> play("<k ><[--]:[---]:><--o->")
 n1 >> play("..I.", drive=(0,0.9), sample=1, rate=(PWhite(-1,4)), amp=PBern(16), pan=linvar([-1,1],24))
-d4 >> play("x(---([::]:[::::]))", amp=3, sample=([0,1,2],[1,2,4])).sometimes("stutter")
-d5 >> play("X ", amp=2)
+d4 >> play("x(---([::]:[::::]))", amp=3).sometimes("stutter")
 
 #### Autobahn !!!!  Pause clopes, bières, on discute du match de la veille #####
 
-PHASE III
-
-Phase Piano
-#Transition à définir
-d_all.stop()
+PHASE III // Piano
 
 v1 >> pianovel([0,4,4,3,[4,4,[2,3],[5,6]]], dur=PBeat("x x xxx xx"), amp=0.5, scale=Scale.minorPentatonic).only()
 v2 >> pianovel(dur=v1.dur, oct=[6,5,5,4], scale=Scale.minorPentatonic, amp=0.3).follow(v1)
@@ -46,20 +33,15 @@ v3 >> pianovel(dur=PBeat(" x x   x  "), oct=[4, 4, 3, 4], scale=Scale.blues, amp
 v4 >> pianovel(dur=v3.dur, oct=[7,6,5,5], scale=Scale.blues, sus=[0.1, 0.5], velocity=[120, PWhite(80, 100)], hard=0.4, amp=0.8).follow(v2)
 v5 >> pasha(PZip([0, 4, 4], [3, 5, [6, 7]]), dur=8, sus=8, delay=0.5, scale=Scale.minorPentatonic, drive=1, shape=8, shapemix=0.2, lpf=2400).spread()
 
-Group(v_all).lpf = 500
-
 l1 >> pianovel(dur=1/2, oct=[2,3], scale=Scale.blues, velocity=[120, PWhite(80, 100)], hard=1)
 l2 >> pianovel(dur=1/2, oct=[4,5,3,4], scale=Scale.blues, velocity=[120, PWhite(120, 100)], hard=[1, 1.4])
 b3 >> dbass(dur=1/2, oct=[4,5,5,5.02], scale=Scale.blues, amp=3)
 
-Group(v_all).lpf = 2500
 
 
-d7 >> play("X:", amp=3)
-Clock.bpm=linvar([120,180],[128,inf], start=now)
-Group(v_all, l_all).stop()
 
-Phase IV Dark teck
+
+Phase IV Dark teck (bpm=128)
 
 b9 >> dirt([0,0,0.5], dur=PDur(3,8), sus=1, chop=4, formant=var([0,1,PRand(8)],[4,3,1])).spread()
 c1 >> play("T", dur=var([1/4,8],[8,1]), sample=P[:8])
@@ -74,12 +56,12 @@ Clock.bpm=var([120,60],[28,4])
 d4 >> play("u", rate=-0.5, dur=PDur([3,5],8)*0.5, pshift=var(P[1:32],[1])+(0.1,0), room=1, shape=2, shapemix=0.5)
 d5 >> play("V ", lpf=900)
 
-Phase IV FINAL
 
-####### Proposition 0, on tweak le tout pour faire crasher
+Phase V Assaut FINAL (BPM =180)
 
-##########Proposition I
+####### Proposition 1, on tweak le tout pour faire crasher
 
+##########Proposition 2
 b5 >> dbass((PSine(28)*PWhite(-0.4, 0.3), PSine(linvar([1, -1]))*2), amp=4, lpf=4500, dur=PDur([4, 5], 8)).spread()
 b8 >> blip([0, 1, 3, 2], oct=(4,6), scale=Scale.diminished, dur=1/4, hpf=0, bpm=linvar([170, 190], [8]), drive=0.4, drivemix=0.4, amp=4, fmod=var([0, 4], [32]))
 t1 >> play("N ", sample=1, dur=1/4, lfp=1020, bpm=linvar([120, 180], [8]))
@@ -90,12 +72,10 @@ t5 >> play("s ", dur=1/4, bpm=linvar([170, 190], [8]))
 t6 >> play("v ", dur=1/4, bpm=linvar([120, 180], [8]))
 
 
-### Proposition II
-#à retravailler
-
+### Proposition 3
 n3 >> play("j", sample=4, amp=2, rate=(0.5 + PSine(16)/100,-0.25), drive=PWhite(0,0.2), chop=PWhite(0,4), dur=4, room=1, lpf=2900).spread()
-b4 >> dbass(var([5,7,-2,2],16), dur=var([1/2,1/4],[12,4]), amp=1, lpf=900).every(8, "offadd", 2).every(12, "shufflets", 2)
-q1 >> play(PEuclid2(3,8,"X","|=2|"), sample=1, drive=(0,0.8),rate=var([1,0.7],[16,2]),lpf=linvar([800,9800],[24,0]), lpr=linvar([1,0.05],[24,0])).often("stutter", Cycle([2,3,12]), amp=1).sometimes("amen")
+b4 >> dbass(var([5,7,-2,2],16), dur=var([1/2,1/4],[12,4]), lpf=900).every(8, "offadd", 2).every(12, "shufflets", 2)
+q1 >> play(PEuclid2(3,8,"X","|=2|"), sample=1, drive=(0,0.8),rate=var([1,0.7],[16,2]))
 q2 >> play("X:", amp=2, sample=2)
-q3 >> play("<V(-[VX])V-><--I->", sample=3, amp=1, hpf=45).every(7, "stutter", Cycle([2, 3, 1, 2, 3, 4]))
-r1 >> rave(dur=PDur(3,var([4,8],[4,5])), amp=2, slidefrom=PWhite(-0.5,0.5), lpf=linvar([900,2500],[8,0])).spread().rarely("offadd", 1, 0.75).degrade(0.2)
+q3 >> play("<V(-[VX])V-><--I->", sample=3, amp=1, hpf=45)
+r1 >> rave(dur=PDur(3,8), amp=2, slidefrom=PWhite(-0.5,0.5)).spread()
