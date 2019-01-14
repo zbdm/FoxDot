9f56628a1bcbff48b28861e960edf9ccb6eafa55
Zbdm <39557845+zbdm@users.noreply.github.com>
2019-01-14 11:19:00 +0100

    Update startup.py

--- FoxDot/lib/Custom/startup.py
+++ FoxDot/lib/Custom/startup.py
@@ -1,1 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+
+
+FOXDOT_SND   = os.path.realpath(FOXDOT_ROOT + "/crashsnd/")
+FOXDOT_LOOP  = os.path.realpath(FOXDOT_ROOT + "/crashsnd/_loop_/")
 
