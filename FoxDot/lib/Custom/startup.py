706107a21ed9f5bd008693dde5ec74acf5042629
zbdm <lagouttedo@gmail.com>
2019-02-22 12:36:10 +0100

    remove unused, make snd default dir. can now install via python setup.py install

--- FoxDot/lib/Custom/startup.py
+++ FoxDot/lib/Custom/startup.py
@@ -6,27 +6,14 @@ import os
 import sys
 
 ## Path Snd
-FOXDOT_SND   = os.path.realpath(FOXDOT_ROOT + "/crashsnd_mod/")
-FOXDOT_LOOP  = os.path.realpath(FOXDOT_ROOT + "/crashsnd/_loop_/")
-FoxDotCode.use_sample_directory(FOXDOT_SND)
+#FOXDOT_SND   = os.path.realpath(FOXDOT_ROOT + "/crashsnd_mod/")
+#FOXDOT_LOOP  = os.path.realpath(FOXDOT_ROOT + "/crashsnd/_loop_/")
+#FoxDotCode.use_sample_directory(FOXDOT_SND)
 
 #OSC VIDEO FORWARD
 #my_client = OSCClient()
 #my_client.connect(("127.0.0.1", 12345)) # Video OSC Ip:port
 #DefaultServer.forward = my_client
-
-### Effects
-#Dist mod
-#    fx = FxList.new('disto', 'disto_mod', {'disto': 0, 'smooth': 0.3, 'distomix': 1}, order=1)
-#    fx.add("osc = LinXFade2.ar(CrossoverDistortion.ar(osc, amp:0.5*disto, smooth:smooth),  osc, 1-distomix)")
-#    fx.save()
-
-#New Chop, with wave select :
-#chopwave = (0: Pulse, 1: Tri, 2: Saw, 3: Sin, 4: Parabolic )
-# and chopi = oscillator phase
-# fx = FxList.new('chop', 'chop', {'chop': 0, 'sus': 1, 'chopmix': 1, 'chopwave': 0, 'chopi': 0}, order=2)
-# fx.add("osc = LinXFade2.ar(osc * SelectX.kr(chopwave, [LFPulse.kr(chop / sus, iphase:chopi, add: 0.01), LFTri.kr(chop / sus, iphase:chopi, add: 0.01), LFSaw.kr(chop / sus, iphase:chopi, add: 0.01), FSinOsc.kr(chop / sus, iphase:chopi, add: 0.01), LFPar.kr(chop / sus, iphase:chopi, add: 0.01)]), osc, 1-chopmix)")
-# fx.save()
 
 ##### SDur by Quantuum #####
 def SDur(target):
